e72be5b354aa506da5d94cc03dd3eef3f309ce51
shingo243 <21990624+shingo243@users.noreply.github.com>
2018-07-26 20:25:52 +0900

    Update sample1.py

--- sample1.py
+++ sample1.py
@@ -23,8 +23,8 @@ while True:
     if data is not None:
 
         if data.tick_last_update > latest_update:
-            print('Illumination: {} lx'.format(data.val_light))
-
+#            print('Illumination: {} lx'.format(data.val_light))
+            print('{} lx'.format(vars(data)))
         latest_update = data.tick_last_update
 
     time.sleep(CHECK_SPAN)
